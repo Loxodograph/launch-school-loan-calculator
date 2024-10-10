7b87d7688475e80ecfb3c884466dacd88df563d0
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2024-10-10 13:15:12 +0000

    functionality for requesting loanAmountValue input, check if number is valid

--- loanCalculator.js
+++ loanCalculator.js
@@ -1,5 +1,23 @@
 const READLINE = require('readline-sync');
+const MESSAGES = require('./messages.json')
 
 function prompt(message) {
-  console.log(`=> ${message}`)
+  console.log(`=> ${MESSAGES[message]}`);
 }
+
+function invalidLoanNumber(number) {
+  return number.trimStart() === "" || Number.isNaN(Number(number)) || parseFloat(number) <= 0;
+} //checks if input number is empty, not a number, or less than zero
+
+prompt('greeting');
+
+prompt('loanAmount');
+
+let loanAmountValue = READLINE.question();
+
+while(invalidLoanNumber(loanAmountValue)){
+  prompt('invalidNumber');
+  loanAmountValue = READLINE.question();
+}
+
+
