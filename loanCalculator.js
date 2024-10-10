a19e6d7b55f6c7e5aa7fc92a9fcbb525d8414857
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2024-10-10 13:40:29 +0000

    completed loan calculator

--- loanCalculator.js
+++ loanCalculator.js
@@ -1,23 +1,60 @@
 const READLINE = require('readline-sync');
-const MESSAGES = require('./messages.json')
+const MESSAGES = require('./messages.json');
 
 function prompt(message) {
   console.log(`=> ${MESSAGES[message]}`);
 }
 
-function invalidLoanNumber(number) {
-  return number.trimStart() === "" || Number.isNaN(Number(number)) || parseFloat(number) <= 0;
+function invalidNumber(number) {
+  number = parseFloat(number);
+  return Number.isNaN(Number(number)) || parseFloat(number) <= 0;
 } //checks if input number is empty, not a number, or less than zero
 
 prompt('greeting');
 
-prompt('loanAmount');
+//calculator loop
+while (true) {
+  prompt('loanAmount');
 
-let loanAmountValue = READLINE.question();
+  let loanAmountValue = READLINE.question();
 
-while(invalidLoanNumber(loanAmountValue)){
-  prompt('invalidNumber');
-  loanAmountValue = READLINE.question();
+  while (invalidNumber(loanAmountValue)) {
+    prompt('invalidNumber');
+    prompt('loanAmount');
+    loanAmountValue = READLINE.question();
+  }
+
+  prompt('APRrequest');
+  let APRValue = READLINE.question();
+
+  while (invalidNumber(APRValue)) {
+    prompt('invalidNumber');
+    prompt('APRrequest');
+    APRValue = READLINE.question();
+  }
+
+  let APRPercentage = APRValue / 100
+  let monthlyInterestRate = APRPercentage / 12
+
+  prompt('loanDuration');
+  let loanDuration = READLINE.question();
+
+  while (invalidNumber(loanDuration)) {
+    prompt('invalidNumber');
+    prompt('loanDuration');
+    APRValue = READLINE.question();
+  }
+
+  let monthlyPayment = loanAmountValue * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))));
+
+  prompt('monthlyPayment');
+  console.log(`=> $${monthlyPayment.toFixed(2)}`);
+
+  prompt("continue");
+  let answer = READLINE.question();
+
+  if (answer[0].toLowerCase() !== 'y') {
+    console.log("=> Goodbye")
+    break;
+  }
 }
-
-
